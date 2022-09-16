846a8d66eca129ec92ac91463bb732a80dd23dce
robzor92 <robin.eric.andersson@gmail.com>
2022-09-16 10:50:32 +0200

    changes

--- code/git/checkout_code.py
+++ code/git/checkout_code.py
@@ -5,7 +5,7 @@ project = hopsworks.login(host="2176a0f0-3503-11ed-be64-b1a4781e5f0a.cloud.hopsw
 git_api = project.get_git_api()
 
 REPO_URL="https://github.com/robzor92/Hopsworks-CI-CD.git" # git repository
-HOPSWORKS_FOLDER="Resources" # path in hopsworks filesystem to clone to
+HOPSWORKS_FOLDER="Jupyter" # path in hopsworks filesystem to clone to
 PROVIDER="GitHub"
 BRANCH="master" # optional branch to clone
 

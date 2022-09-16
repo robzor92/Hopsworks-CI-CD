a4686c1ee02545cddd77776afa04428635a426b1
robzor92 <robin.eric.andersson@gmail.com>
2022-09-16 10:45:49 +0200

    changes

--- code/git/checkout_code.py
+++ code/git/checkout_code.py
@@ -1,6 +1,6 @@
 import hopsworks
 
-project = hopsworks.login()
+project = hopsworks.login(host="2176a0f0-3503-11ed-be64-b1a4781e5f0a.cloud.hopsworks.ai")
 
 git_api = project.get_git_api()
 
@@ -9,4 +9,9 @@ HOPSWORKS_FOLDER="Resources" # path in hopsworks filesystem to clone to
 PROVIDER="GitHub"
 BRANCH="master" # optional branch to clone
 
-examples_repo = git_api.clone(REPO_URL, HOPSWORKS_FOLDER, PROVIDER, branch=BRANCH)
+
+repo = git_api.get_repo("Hopsworks-CI-CD")
+if repo is not None:
+    repo.pull("master")
+else:
+    examples_repo = git_api.clone(REPO_URL, HOPSWORKS_FOLDER, PROVIDER, branch=BRANCH)
